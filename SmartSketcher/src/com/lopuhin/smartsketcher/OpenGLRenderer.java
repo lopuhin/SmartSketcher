3fbeebca4bb49d7cac4c41ff07d7c88ef9949381
Kostia Lopuhin <kostia.lopuhin@gmail.com>
2012-01-10 01:46:48 +0400

    use ortho projection, remove unneseccary variables

--- SmartSketcher/src/com/lopuhin/smartsketcher/OpenGLRenderer.java
+++ SmartSketcher/src/com/lopuhin/smartsketcher/OpenGLRenderer.java
@@ -129,7 +129,9 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     }
 
     public void onDrawFrame(GL10 unused) {
-    
+        /**
+         * Called on every frame redraw
+         */
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
         
@@ -137,41 +139,15 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         final PointF ul, lr; // upper left and lower right
         ul = sheet.getViewPos();
         lr = sheet.toSheet(new PointF(width, height));
-        
-        Log.d(TAG, "ul " + ul.x + " " + ul.y + 
-              " lr " + lr.x + " " + lr.y);
-        
-        // Create a new perspective projection matrix
-        // final float ratio = (float) width / height;
-        final float left = ul.x;
-        final float right = lr.x;
-        final float bottom = lr.y;
-        final float top = ul.y;
-        final float near = 50.0f;
-        final float far = 1000.0f; // TODO - use ortho
-
-        Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
+        final float left = ul.x, right = lr.x;
+        final float bottom = lr.y, top = ul.y;
+        final float near = 50.0f, far = 1000.0f;
+        Matrix.orthoM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
     
-        // Position the eye behind the origin.
-        final float eyeX = 0.0f;
-        final float eyeY = 0.0f;
-        final float eyeZ = 100.0f;
-
-        // We are looking toward the distance
-        final float lookX = 0.0f;
-        final float lookY = 0.0f;
-        final float lookZ = -5.0f;
-
-        // Set our up vector. This is where our head would be pointing
-        // were we holding the camera.
-        final float upX = 0.0f;
-        final float upY = 1.0f;
-        final float upZ = 0.0f;
-
         Matrix.setLookAtM(mVMatrix, 0,
-                          eyeX, eyeY, eyeZ,
-                          lookX, lookY, lookZ,
-                          upX, upY, upZ);
+                          0.0f, 0.0f, 100.0f, // eye
+                          0.0f, 0.0f, -1.0f,  // look
+                          0.0f, 1.0f, 0.0f);  // up
                    
         Matrix.setIdentityM(mModelMatrix, 0);
         // This multiplies the view matrix by the model matrix,
