3e128cae4877337ddca58c09c9e7293cb4923108
Kostia Lopuhin <kostia.lopuhin@gmail.com>
2012-01-14 18:51:13 +0400

    proper drawing of erase trace using triangle strip

--- SmartSketcher/src/com/lopuhin/smartsketcher/EraseTrace.java
+++ SmartSketcher/src/com/lopuhin/smartsketcher/EraseTrace.java
@@ -12,22 +12,29 @@ public class EraseTrace extends Shape {
     /**
      * A trace of an eraser
      */
-    private PointF[] points;
-    private FloatBuffer pointsBuffer;
-    private float thickness;
+    private final PointF[] points;
+    private final FloatBuffer pointsBuffer;
+    private final FloatBuffer boundaryBuffer;
+    private final int boundarySize;
+    private final float thickness;
 	
     EraseTrace(final ArrayList<PointF> points, final float thickness) {
-        this.points = points.toArray(new PointF[points.size()]);
         this.thickness = thickness;
-        pointsBuffer =
-            OpenGLRenderer.createBuffer(this.points, Color.RED);
+        this.points = points.toArray(new PointF[points.size()]);
+        pointsBuffer = OpenGLRenderer.createBuffer(this.points, Sheet.BACKGROUND_COLOR);
+        PointF[] boundary = createBoundary(this.points, this.thickness);
+        boundarySize = boundary.length;
+        boundaryBuffer = OpenGLRenderer.createBuffer(boundary, Sheet.BACKGROUND_COLOR);
     }
 	
     public void draw(OpenGLRenderer renderer) {
-        renderer.drawArray(pointsBuffer, points.length, GLES20.GL_LINE_STRIP);
-        //renderer.drawArray(pointsBuffer, points.length, GLES20.GL_TRIANGLE_STRIP);
-        if (Config.DEBUG)
+        if (Config.DEBUG) {
+            renderer.drawArray(pointsBuffer, points.length, GLES20.GL_LINE_STRIP);
             renderer.drawArray(pointsBuffer, points.length, GLES20.GL_POINTS);
+        }
+        if (boundarySize >= 3)
+            renderer.drawArray(boundaryBuffer, boundarySize, GLES20.GL_TRIANGLE_STRIP);
+        // TODO - draw half-circles or one circle (if only one point)
     }
 
     @Override
@@ -38,5 +45,27 @@ public class EraseTrace extends Shape {
     @Override
     public float getThickness() {
         return thickness;
+    }
+
+    private static PointF[] createBoundary(final PointF[] points, final float thickness) {
+        /**
+         * Create the boundary of the erase trace
+         */
+        ArrayList<PointF> boundary = new ArrayList<PointF>();
+        PointF prev = null;
+        PointF v1 = null, v2 = null;
+        for (PointF curr: points) {
+            if (prev != null) {
+                final PointF conn = Vector.sub(curr, prev);
+                final PointF orth = Vector.normalized(Vector.orth(conn), thickness / 2.0f);
+                boundary.add(Vector.add(prev, orth));
+                boundary.add(Vector.sub(prev, orth));
+                // FIXME - maybe add only once for point?
+                boundary.add(Vector.add(curr, orth));
+                boundary.add(Vector.sub(curr, orth));
+            }
+            prev = curr;
+        }
+        return boundary.toArray(new PointF[boundary.size()]);
     }
 }
--- SmartSketcher/src/com/lopuhin/smartsketcher/Vector.java
+++ SmartSketcher/src/com/lopuhin/smartsketcher/Vector.java
@@ -5,19 +5,30 @@ import android.util.FloatMath;
 
 
 class Vector {
+    /**
+     * Some vector utils
+     */
+    
     public static PointF translated(PointF point, PointF shift, float cos, float sin) {
         /**
-         * Return @point, moved by vector @shift and rotated by angle given by cos and sin
+         * Return @point, moved by vector @shift and
+         rotated by angle (given by @cos and @sin) around the origin
          */
         PointF p = new PointF(point.x + shift.x, point.y + shift.y);
         return new PointF(p.x * cos + p.y * sin, -p.x * sin + p.y * cos);
     }
 
     public static float norm(final PointF p) {
+        /**
+         * Euclidian norm
+         */
         return FloatMath.sqrt(p.x * p.x + p.y * p.y);
     }
         
     public static PointF normalized(final PointF p, final float newNorm) {
+        /**
+         * Vecor with the same direction as @p and norm @newNorm
+         */
         final float norm = norm(p);
         if (norm < 0.0001f) {
             return new PointF();
@@ -26,4 +37,17 @@ class Vector {
             return new PointF(p.x * coef, p.y * coef);
         }
     }
+
+    public static PointF sub(final PointF v1, final PointF v2) {
+        return new PointF(v1.x - v2.x, v1.y - v2.y);
+    }
+
+    public static PointF add(final PointF v1, final PointF v2) {
+        return new PointF(v1.x + v2.x, v1.y + v2.y);
+    }
+
+    public static PointF orth(final PointF v) {
+        return new PointF(-v.y, v.x);
+    }
+
 }
